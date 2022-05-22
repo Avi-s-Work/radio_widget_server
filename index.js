a8e8d0bfe854f23501f3fe4ccd4ff4a9fca89b89
avishekataur <avishekataur@gmail.com>
2022-05-22 12:41:48 +0600

    heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || 5000;
 
 //middleware
